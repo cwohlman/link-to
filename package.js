aedb2ab00465b70395fc466a28a9d0dcd145a6a1
Joshua Ohlman <codewriterohs@gmail.com>
2014-11-05 15:44:33 -0600

    Add package name

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   summary: "A simple helper for iron router to create route links.",
   version: "1.0.0",
-  git: "https://github.com/cwohlman/link-to.git"
+  git: "https://github.com/cwohlman/link-to.git",
+  name: "cwohlman:link-to"
 });
 
 Package.on_use(function(api) {
   api.use('ui');
   api.use('templating');
-  
+
   api.add_files('link_to.html', ['client']);
   api.add_files('link_to.js', ['client']);
 });
-
