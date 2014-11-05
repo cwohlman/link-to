2ee33df52c656a7566dbf6324692f6e8f8e6f455
Joshua Ohlman <codewriterohs@gmail.com>
2014-11-05 15:46:26 -0600

    Publish version v1.0.1

--- package.js
+++ package.js
@@ -1,11 +1,13 @@
 Package.describe({
   summary: "A simple helper for iron router to create route links.",
-  version: "1.0.0",
+  version: "1.0.1",
   git: "https://github.com/cwohlman/link-to.git",
   name: "cwohlman:link-to"
 });
 
 Package.on_use(function(api) {
+  api.versionsFrom('METEOR@1.0');
+
   api.use('ui');
   api.use('templating');
 
